f047d2b83d70d61d43f1def144c06dabb34f7b35
BuildTools <unconfigured@null.spigotmc.org>
2018-08-04 01:05:40 +0200

    fix: Fix: ajout constantes pour les Font
    rcz: MenuPanel: bug pour les Font auto-fixed ???????

--- src/fix/Fix.java
+++ src/fix/Fix.java
@@ -2,9 +2,14 @@ package fix;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.Font;
 
 public class Fix {
 	public static final String WINDOW_NAME= "The Drah-Nok Invasion";
 	public static final Dimension WINDOW_SIZE= Toolkit.getDefaultToolkit().getScreenSize();
+	
 	public static final int TARGETED_FPS= 60;
+
+	public static final Font TITLE_FONT= new Font("TimesRoman", Font.ITALIC,16);
+	public static final Font DEFAULT_FONT= new Font("Dialog", Font.PLAIN,12);
 }
--- src/rcz/MenuPanel.java
+++ src/rcz/MenuPanel.java
@@ -12,23 +12,20 @@ public class MenuPanel extends JPanel{
 	private String title;
 	private String[] choice;
 	private int select=0;
-	private Font defFont= new Font("Dialog", Font.PLAIN,12);
-	private Font titleFont= new Font("TimesRoman", Font.ITALIC,16);
 	
 	public MenuPanel(String t, String[] c) {
 		this.setPreferredSize(Fix.WINDOW_SIZE);
 		title= t;
 		choice= c;
-		
+		//this.setFont(new Font("Dialog", Font.PLAIN,32));
+		this.setFont(Fix.TITLE_FONT);
 	}
 	
 	public void paintComponent(Graphics g) {
 		g.setColor(Color.BLACK);
 		
-		this.setFont(titleFont);
-		g.drawString(title, this.getWidth()/2, 50);
 		
-		this.setFont(defFont);
+		g.drawString(title, this.getWidth()/2, 50);
 		for(int i=0; i<choice.length; i++) {
 			g.drawString((i==select)?"*"+choice[i]+"*":choice[i], this.getWidth()/2, 75+15*i);
 		}
